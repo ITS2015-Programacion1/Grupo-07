d2e1c33486d76bab67b3f5876ec0a6614d969c15
Marcos Corvalán <marcoscorva@hotmail.com>
2015-11-23 21:24:04 -0300

    Fisica, camara y fondo.
    Se agrego un fondo aunque de manera erronea ya que la imagen no se ajusta a la pantalla de la terminal al ejecutarse el juego, lo que sera corregido posteriormente. Se intento que la camara siga al actor Futbol pero no se logro. Se agregaron tres colisiones a modo de prueba. Y sobretodo se intento que que los actores rebotaran entre si fisicamente sin ningun otro tipo de interaccion entre ellos, a travez de rectangules y circulos de fisica que proporciona pilas pero no se lo logro.

--- Juego_Cabezones.py
+++ Juego_Cabezones.py
@@ -5,7 +5,15 @@ sys.path.append('.')
 import pilasengine
 
 pilas = pilasengine.iniciar()
-fondo = pilas.fondos.Espacio()
+#pilas.depurador.definir_modos(fisica=True)
+
+fondo = pilas.fondos.Fondo()
+fondo.imagen = pilas.imagenes.cargar('cancha.png')
+
+fondo.imagen.repetir_vertical = True
+fondo.imagen.repetir_horizontal = True
+
+pilas.fisica.eliminar_paredes()
 
 class SaltarUnaVez(pilas.comportamientos.Comportamiento):
     """Realiza un salto, cambiando los atributos 'y'."""
@@ -48,20 +56,20 @@ teclas = {
             pilas.simbolos.s: 'abajo',
             pilas.simbolos.ESPACIO: 'boton',
         }
+
 mi_control = pilas.control.Control(teclas)
 """Del ejemplo 'control_personalizado.py'"""
 
 
 
-class AceitunaConControles(pilasengine.actores.aceituna.Aceituna):
+class AceitunaConControles(pilasengine.actores.Actor):
 
     def iniciar(self):
         self.imagen = "aceituna.png"
         self.escala = 1.2
-	    aceituna_con_controles.y -= 200
+        self.radio_de_colision= 21
+
         self.saltando = False
-
-
 
     def actualizar(self):
         if mi_control.izquierda:
@@ -76,23 +84,23 @@ class AceitunaConControles(pilasengine.actores.aceituna.Aceituna):
                 self.hacer("SaltarUnaVez")
         elif mi_control.abajo:	
             self.y -=1
-
-
-
+    
 
 aceituna_con_controles = AceitunaConControles(pilas)
+#aceituna_con_controles.aprender(pilas.habilidades.RebotarComoPelota)
+aceituna_con_controles.y -= 150
+aceituna_con_controles.x -= 100
 
-
+'''circulo = pilas.fisica.Circulo(0, 0, 15, dinamica=True)
+aceituna_con_controles.aprender(pilas.habilidades.Imitar, circulo)'''
 
 class MonoConControles(pilasengine.actores.mono.Mono):
 
     def iniciar(self):
         self.imagen = "mono.png"
         self.escala = .4
-        aceituna_con_controles.y -= 200
-        self.radio_de_colision=15
+        self.radio_de_colision=21
         
-
         self.saltando = False
 
     def actualizar(self):
@@ -106,7 +114,54 @@ class MonoConControles(pilasengine.actores.mono.Mono):
                 self.hacer("SaltarUnaVez")
 
 
+mono_con_controles = MonoConControles(pilas)
+#mono_con_controles.aprender(pilas.habilidades.RebotarComoPelota)
+mono_con_controles.y -= 150
+mono_con_controles.x = 100
 
 pilas.comportamientos.vincular(SaltarUnaVez)
-mono_con_controles = MonoConControles(pilas)
+
+class Pelota(pilasengine.actores.Actor):
+    
+        def iniciar(self):
+            self.imagen = "futbol.png"
+            self.escala = 0.115
+            self.radio_de_colision=180
+
+            #self.figura.sin_rotacion = True
+            #self.figura.escala_de_gravedad = 2
+
+
+Futbol = Pelota(pilas)
+Futbol.aprender(pilas.habilidades.RebotarComoPelota)
+#La siguiente habilidad se le agrega al actor unicamente para una prueba respecto a la camara
+Futbol.aprender(pilas.habilidades.Arrastrable)
+
+
+def Choque (Futbol, mono_con_controles):
+    print "Funciona"
+
+def Patada (Futbol, aceituna_con_controles):
+    print "Funciona 2"    
+
+def Falta (aceituna_con_controles, mono_con_controles):
+    print "Funciona 3"
+
+
+
+'''rect1=pilas.fisica.Rectangulo(-150,-100,50,50)
+aceituna_con_controles.aprender(pilas.habilidades.Imitar,rect1)
+
+rect2=pilas.fisica.Rectangulo(-150,-100,50,50)
+mono_con_controles.aprender(pilas.habilidades.Imitar,rect2)'''
+
+
+pilas.colisiones.agregar(Futbol, mono_con_controles, Choque)
+pilas.colisiones.agregar(Futbol, aceituna_con_controles, Patada)
+pilas.colisiones.agregar(aceituna_con_controles, mono_con_controles, Falta)
+
+camara = pilas.escena_actual().camara
+camara.x = [Futbol.x]
+
+pilas.fisica.Rectangulo(0, -220, 635, 68, sensor=False, dinamica=False)
 pilas.ejecutar()
